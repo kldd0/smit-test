2b117d43eea5bc9b1beea9f5d36dcdfa1438879a
Stepan <steparesh@gmail.com>
2023-07-14 16:41:12 +0300

    Initial state

---
+++ app/__main__.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+
+# from app import api
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    # app.include_router() # api.router
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    # register tortoise orm
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.__main__:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        reload_dirs=["app", "tests"],
+        log_level="debug",
+    )
